ce6b20f219b6145700eb58e6bd394139dc8d5476
James Hill <395+jameshill@users.noreply.github.com.>
2024-04-03 20:05:40 +1100

    Basic setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
 require "buildkite/test_collector"
 
-Buildkite::TestCollector.configure(hook: :rspec)
+Buildkite::TestCollector.configure(
+  hook: :rspec,
+  url: "http://analytics-api.buildkite.localhost/v1/uploads",
+  token: "xx-local-analytics-key",
+)
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,12 @@
+require "spec_helper"
+
 describe "Simple integers" do
 
   it "should equal themselves" do
     expect(2).to eq(2)
   end
 
+  it "should equal themselves" do
+    expect(2).to eq(3)
+  end
 end
