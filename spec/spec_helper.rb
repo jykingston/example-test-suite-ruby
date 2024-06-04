c87c2837b61154863502a9002c487bb40d8de6e7
James Hill <395+jameshill@users.noreply.github.com.>
2024-06-04 16:17:22 +1000

    Programmatically skipping rspec tests from YAML file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'yaml'
 require "buildkite/test_collector"
 
 Buildkite::TestCollector.configure(
@@ -5,3 +6,16 @@ Buildkite::TestCollector.configure(
   # url: "http://analytics-api.buildkite.localhost/v1/uploads",
   token: ENV["BUILDKITE_ANALYTICS_TOKEN"]
 )
+
+skip = YAML.load_file('skip.yml')
+
+skip_location = skip['location'].reduce({}) { |m, t| m[t] = true; m }
+skip_description = skip['description'].reduce({}) { |m, t| m[t] = true; m }
+
+RSpec.configure do |c|
+  c.around :example do |example|
+    next if skip_location[example.location]
+    next if skip_description[example.full_description]
+    example.run
+  end
+end
