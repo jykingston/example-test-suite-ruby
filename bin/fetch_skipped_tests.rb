871208496a5ce9550100faa28389c3f1a706001f
James Hill <395+jameshill@users.noreply.github.com.>
2024-06-19 22:33:36 +1000

    Fetch skipped tests

---
+++ bin/fetch_skipped_tests.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+require 'net/http'
+require 'uri'
+require 'json'
+require 'csv'
+
+spec_files = Dir["spec/**/*.rb"]
+
+# Define the URL and Bearer token
+url = "https://api.buildkite.com/v2/analytics/organizations/jimjams/suites/ta-example/tests/disabled"
+bearer_token = ENV["API_ACCESS_TOKEN"]
+
+if bearer_token.nil?
+  puts "No API Access Token"
+  return
+end
+
+
+uri = URI.parse(url)
+request = Net::HTTP::Get.new(uri)
+request["Authorization"] = "Bearer #{bearer_token}"
+request["Content-Type"] = "application/json"
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+File.open("skipped.json", 'w') do |file|
+  file.puts(response.body)
+end
