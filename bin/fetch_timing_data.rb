808b010b5a0383c3840f42eda2038ee8ce74084a
James Hill <395+jameshill@users.noreply.github.com>
2024-07-08 16:18:17 +1000

    Update fetch_timing_data.rb to test v2-ingestion

--- bin/fetch_timing_data.rb
+++ bin/fetch_timing_data.rb
@@ -8,7 +8,7 @@ require 'csv'
 spec_files = Dir["spec/**/*.rb"]
 
 # Define the URL and Bearer token
-url = "https://api.buildkite.com/v2/analytics/organizations/jimjams/suites/ta-example/test_files"
+url = "https://api.buildkite.com/v2/analytics/organizations/jimjams/suites/v2-ingestion/test_files"
 bearer_token = ENV["API_ACCESS_TOKEN"]
 
 if bearer_token.nil?
