2ad200bfb95fcb89bfd387602273946b83d94ce4
James Hill <395+jameshill@users.noreply.github.com.>
2024-07-26 16:59:04 +1000

    Add more runtime of demo suite

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -2,26 +2,32 @@ require "spec_helper"
 
 describe "User" do
   it "should be honest" do
+    sleep(1)
     expect("honest").to eq("honest")
   end
 
   it "should be candid" do
+    sleep(1)
     expect("candid").to eq("candid")
   end
 
   it "should be kind" do
+    sleep(1)
     expect("kind").to eq("kind")
   end
 
   it "should be curious" do
+    sleep(1)
     expect("curious").to eq("curious")
   end
 
   it "should be direct" do
+    sleep(1)
     expect("direct").to eq("direct")
   end
 
   it "should be tenacious" do
+    sleep(1)
     expect("tenacious").to eq("tenacious")
   end
 end
