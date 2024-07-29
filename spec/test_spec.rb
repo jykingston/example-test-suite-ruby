53ef5154bf048df681467aad1c8b1b56549d779b
James Hill <395+jameshill@users.noreply.github.com.>
2024-07-29 15:23:25 +1000

    Remove Duplicate Test
    This test has the exact same scope & name, so it's not differentiated
    in Test Analytics

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -5,10 +5,6 @@ describe "Simple integers" do
     expect(2).to eq(2)
   end
 
-  it "should equal themselves" do
-    expect(2).to eq(2)
-  end
-
   # it "random" do
   #   expect(rand(1)).to eq(rand(2))
   # end
